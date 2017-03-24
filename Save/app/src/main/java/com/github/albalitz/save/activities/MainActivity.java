9c6373a72ab498dc6b103db45787ed7fa3ad8930
albalitz <albalitz@fb3.uni-bremen.de>
2017-03-24 18:39:15 +0100

    Use util function for snackbar

--- Save/app/src/main/java/com/github/albalitz/save/activities/MainActivity.java
+++ Save/app/src/main/java/com/github/albalitz/save/activities/MainActivity.java
@@ -2,15 +2,15 @@ package com.github.albalitz.save.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.github.albalitz.save.R;
 import com.github.albalitz.save.utils.ActivityUtils;
+import com.github.albalitz.save.utils.Utils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,8 +25,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Utils.showSnackbar(view, "TODO: SAVE STUFF");
             }
         });
     }
