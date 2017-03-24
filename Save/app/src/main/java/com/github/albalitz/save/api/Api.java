1118f348bc0dccc7a1d085d5c0ed7dc841dfa12a
albalitz <albalitz@fb3.uni-bremen.de>
2017-03-24 18:31:04 +0100

    Add save.api.Api for API interaction
    This class will provide functions to allow interaction with the API.

---
+++ Save/app/src/main/java/com/github/albalitz/save/api/Api.java
@@ -0,0 +1,51 @@
+package com.github.albalitz.save.api;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import com.github.albalitz.save.SaveApplication;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+/**
+ * Created by albalitz on 3/24/17.
+ */
+public class Api {
+
+    private SharedPreferences prefs;
+    private Activity callingActivity;
+
+    public Api(Activity callingActivity) {
+        this.prefs = SaveApplication.getSharedPreferences();
+        this.callingActivity = callingActivity;
+    }
+
+    public void savedLinks() {
+        String url = this.prefs.getString("pref_key_api_url", null);
+        if (url == null) {
+            Log.e(this.toString(), "No URL set in the preferences!");
+            return;
+        }
+
+        JsonHttpResponseHandler jsonHttpResponseHandler = new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // todo: use this.callingActivity to update the displayed list
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                // todo
+            }
+        };
+        Request.get(url,
+                prefs.getString("pref_key_api_username", null),
+                prefs.getString("pref_key_api_password", null),
+                null,  // request params
+                jsonHttpResponseHandler);
+    }
+}
