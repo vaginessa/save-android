b632ffd7802e81db4a0e5a325954d473d6bb57d0
albalitz <albalitz@fb3.uni-bremen.de>
2017-03-24 18:45:41 +0100

    Add ListView for saved links

--- Save/app/src/main/java/com/github/albalitz/save/activities/MainActivity.java
+++ Save/app/src/main/java/com/github/albalitz/save/activities/MainActivity.java
@@ -8,11 +8,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 import com.github.albalitz.save.R;
+import com.github.albalitz.save.api.Api;
 import com.github.albalitz.save.utils.ActivityUtils;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listViewSavedLinks;
+
+    private Api api;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // assign content
+        listViewSavedLinks = (ListView) findViewById(R.id.listViewSavedLinks);
+
+        // prepare stuff
+        api = new Api(this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
