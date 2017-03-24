58eb6b8e01f24e0877f8712b096c9f3245c48c3c
albalitz <albalitz@fb3.uni-bremen.de>
2017-03-24 15:30:57 +0100

    Add user sub-preferences
    Consisting of
    - username
    - password

--- Save/app/src/main/java/com/github/albalitz/save/fragments/SettingsFragment.java
+++ Save/app/src/main/java/com/github/albalitz/save/fragments/SettingsFragment.java
@@ -26,7 +26,13 @@ public class SettingsFragment extends PreferenceFragment {
      * so the values are visible on the preferences screen.
      */
     private void setValuesAsSummary() {
+        // api url
         EditTextPreference apiUrlEditText = (EditTextPreference) findPreference("pref_key_api_url");
+        apiUrlEditText.setSummary(apiUrlEditText.getSummary() + ": " + apiUrlEditText.getText());
+
+        // api user settings
+        // not displaying anything special for the password
+        EditTextPreference usernameEditText = (EditTextPreference) findPreference("pref_key_api_username");
         apiUrlEditText.setSummary(apiUrlEditText.getSummary() + ": " + apiUrlEditText.getText());
     }
 }
