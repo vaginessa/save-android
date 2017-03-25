ef201ebd64d139b14f48f725e3c732df7cd5c9e6
albalitz <albalitz@fb3.uni-bremen.de>
2017-03-25 12:05:28 +0100

    Allow opening and closing registration activity

--- Save/app/src/main/java/com/github/albalitz/save/activities/RegisterActivity.java
+++ Save/app/src/main/java/com/github/albalitz/save/activities/RegisterActivity.java
@@ -2,14 +2,29 @@ package com.github.albalitz.save.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.github.albalitz.save.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private Button buttonRegister;
+    private Button buttonCancel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(this.toString(), "Creating RegisterActivity.");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        buttonCancel = (Button) findViewById(R.id.buttonCancel);
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
